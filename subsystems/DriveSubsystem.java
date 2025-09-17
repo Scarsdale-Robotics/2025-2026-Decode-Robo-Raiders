9db94a55d500ceab0f80b9d6de8cfcda91a77a9e
Sira0909 <sira6060@proton.me>
2025-09-17 16:26:56 -0400

    slight edits

--- subsystems/DriveSubsystem.java
+++ subsystems/DriveSubsystem.java
@@ -32,4 +32,127 @@ public class DriveSubsystem implements Subsystem {
     this.targetY = 0;
     this.targetH = 0;
   }
+
+
+  /////////////
+  // GETTERS //
+  /////////////
+
+  public double getLeftFrontPosition() {
+    return -leftFront.getCurrentPosition();
+  }
+
+  public double getRightFrontPosition() {
+    return -rightFront.getCurrentPosition();
+  }
+
+  public double getLeftBackPosition() {
+    return leftBack.getCurrentPosition();
+  }
+
+  public double getRightBackPosition() {
+    return -rightBack.getCurrentPosition();
+  }
+  ///////////////////
+  // DRIVE METHODS //
+  ///////////////////
+
+  /**
+   * Drives with directions based on robot pov.
+   *
+   * @param strafe     Strafe power.
+   * @param forward     Forward power.
+   * @param turn      Turn power.
+   */
+  public void driveRobotCentricPowers(double strafe, double forward, double turn) {
+
+  }
+
+  /**
+   * Drives with directions based on robot pov.
+   *
+   * @param theta     Direction of drive in radians.
+   * @param speed     Desired driving speed in in/s.
+   * @param turn      Desired angular velocity in rad/s.
+   */
+  public void driveRobotCentric(double theta, double speed, double turn) {
+    driveFieldCentric(theta, speed, -turn, 0.0);
+  }
+
+  /**
+   * Drives based on driver pov.
+   * @param gyroAngle Robot heading in radians.
+   */
+  public void driveFieldCentric(double gyroAngle) {
+    driveFieldCentric(driveTheta, driveSpeed, turnVelocity, gyroAngle);
+  }
+
+  /**
+   * Drives based on driver pov.
+   *
+   * @param strafe     Strafe power.
+   * @param forward     Forward power.
+   * @param turn      Turn power.
+   * @param gyroAngle Robot heading in radians.
+   */
+  public void driveFieldCentricPowers(double strafe, double forward, double turn, double gyroAngle) {
+  }
+
+  /**
+   * Corrected driving with bias based on driver pov.
+   *
+   * @param theta     Direction of drive in radians.
+   * @param speed     Desired driving speed in in/s.
+   * @param turn      Desired angular velocity in rad/s.
+   * @param gyroAngle Robot heading in radians.
+   */
+  public void driveFieldCentric(double theta, double speed, double turn, double gyroAngle) {
+    theta = normalizeAngle(Math.PI/2 + (theta-gyroAngle));
+    double maxSpeed = Math.hypot(
+        40.0*Math.cos(theta),//Max strafe speed
+        54.0*Math.sin(theta)//Max forward speed
+    );
+
+    double L = 0;
+    double R = 0;
+
+    double theta_w =
+  }
+
+  /**
+   * Stop the motors.
+   */
+  public void stopController() {
+
+  }
+
+
+  /**
+   * Normalizes a given angle to (-pi,pi] radians.
+   * @param radians the given angle in radians.
+   * @return the normalized angle in radians.
+   */
+  private static double normalizeAngle(double radians) {
+
+  }
+
+  /**
+   * Normalize the wheel speeds
+   */
+  private void normalize(double[] wheelSpeeds) {
+
+
+  }
+
+
+  /////////////////////////////////////
+  // AUTOMATIC DRIVING FUNCTIONALITY //
+  /////////////////////////////////////
+  public void stepTowards2DPoint(Point error) {
+    // TODO: IMPLEMENT
+  }
+
+  public void stepTowardsFieldLocation() {
+    // TODO: IMPLEMENT
+  }
 }
