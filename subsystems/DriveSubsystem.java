68427efccd21ae4a5606e241fe36da0cbd0bd30f
Sira0909 <sira6060@proton.me>
2025-09-17 16:05:56 -0400

    started drive

--- subsystems/DriveSubsystem.java
+++ subsystems/DriveSubsystem.java
@@ -1,5 +1,35 @@
-package org.firstinspires.ftc.teamcode.subsystems
+package org.firstinspires.ftc.teamcode.subsystems;
 
-public class DriveSubsystem {
+import dev.nextftc.core.subsystems.Subsystem;
+import dev.nextftc.hardware.impl.MotorEx;
 
+public class DriveSubsystem implements Subsystem {
+  private final MotorEx leftFront;
+  private final MotorEx rightFront;
+  private final MotorEx leftBack;
+  private final MotorEx rightBack;
+
+  public double driveSpeed;
+  public double driveTheta;
+  public double turnVelocity;
+
+  public double targetX;
+  public double targetY;
+  public double targetH;
+  public DriveSubsystem(MotorEx leftFront,
+                        MotorEx rightFront,
+                        MotorEx leftBack,
+                        MotorEx rightBack) {
+    this.leftFront = leftFront;
+    this.rightFront = rightFront;
+    this.leftBack = leftBack;
+    this.rightBack = rightBack;
+
+    this.driveSpeed = 0;
+    this.driveTheta = 0;
+    this.turnVelocity = 0;
+    this.targetX = 0;
+    this.targetY = 0;
+    this.targetH = 0;
+  }
 }
