e5e90da27157c800d6a20d0119c95f1b722ef770
Nathan Tao <natnu212@gmail.com>
2025-09-17 16:32:55 -0400

    hi

---
+++ opmodes/testing/TemplateTeleOp.java
@@ -0,0 +1,10 @@
+package org.firstinspires.ftc.teamcode.opmodes.testing;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import dev.nextftc.ftc.NextFTCOpMode;
+
+@TeleOp(name = "NextFTC TeleOp Program Java")
+public class TemplateTeleOp extends NextFTCOpMode {
+
+}
