d284b8df32b5132e03d40c9b8ca7fcd952420986
Nathan Tao <natnu212@gmail.com>
2025-09-17 16:37:59 -0400

    outtake organization

---
+++ subsystems/OuttakeSubsystem.java
@@ -0,0 +1,8 @@
+package org.firstinspires.ftc.teamcode.subsystems;
+
+import dev.nextftc.core.subsystems.Subsystem;
+
+public class OuttakeSubsystem implements Subsystem {
+    public static final OuttakeSubsystem INSTANCE = new OuttakeSubsystem();
+    private OuttakeSubsystem() { }
+}
--- subsystems/outtake/ShooterSubsystem.java
+++ subsystems/outtake/ShooterSubsystem.java
@@ -1,4 +1,4 @@
-package org.firstinspires.ftc.teamcode.subsystems;
+package org.firstinspires.ftc.teamcode.subsystems.outtake;
 
 import dev.nextftc.core.subsystems.Subsystem;
 
