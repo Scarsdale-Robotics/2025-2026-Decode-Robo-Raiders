1f2a4428a49c6da3130c91070d62e3983ead3eb1
Nathan Tao <natnu212@gmail.com>
2025-09-19 15:20:06 -0400

    yum

--- subsystems/OuttakeSubsystem.java
+++ subsystems/OuttakeSubsystem.java
@@ -8,30 +8,30 @@ import dev.nextftc.core.commands.utility.LambdaCommand;
 import dev.nextftc.core.subsystems.SubsystemGroup;
 
 public class OuttakeSubsystem extends SubsystemGroup {
-  public static final OuttakeSubsystem INSTANCE = new OuttakeSubsystem();
-  private OuttakeSubsystem() {
-    super(
-      ShooterSubsystem.INSTANCE,
-      TurretSubsystem.INSTANCE
-    );
-  }
+    public static final OuttakeSubsystem INSTANCE = new OuttakeSubsystem();
+    private OuttakeSubsystem() {
+        super(
+            ShooterSubsystem.INSTANCE,
+            TurretSubsystem.INSTANCE
+        );
+    }
 
 
-  public void onUpdate(){
-    ShooterSubsystem.INSTANCE.onUpdate();
-    TurretSubsystem.INSTANCE.onUpdate();
+    public void onUpdate(){
+        ShooterSubsystem.INSTANCE.onUpdate();
+        TurretSubsystem.INSTANCE.onUpdate();
 
-  }
+    }
 
 
-  ////////////////////////
-  /// shooter commands ///
-  ////////////////////////
-  public Command shooterOn = new LambdaCommand()
-    .setStart(()->ShooterSubsystem.INSTANCE.setOn(true))
-    .setIsDone(()->true);
-  public Command shooterOff = new LambdaCommand()
-    .setStart(()->ShooterSubsystem.INSTANCE.setOn(false))
-    .setIsDone(()->true);
+    ////////////////////////
+    /// shooter commands ///
+    ////////////////////////
+    public Command shooterOn = new LambdaCommand()
+      .setStart(()->ShooterSubsystem.INSTANCE.setOn(true))
+      .setIsDone(()->true);
+    public Command shooterOff = new LambdaCommand()
+      .setStart(()->ShooterSubsystem.INSTANCE.setOn(false))
+      .setIsDone(()->true);
 
 }
