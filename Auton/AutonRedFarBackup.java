dc8d6a7f420b3bd941cd76eaac9bf84a3f6b721c
Aaron <113719167+DraconicFall@users.noreply.github.com>
2025-09-26 14:50:28 -0400

    yay 4 backup autos done
    les go

--- Auton/AutonRedFarBackup.java
+++ Auton/AutonRedFarBackup.java
@@ -1,5 +1,6 @@
 package org.firstinspires.ftc.teamcode.Auton;
 
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.pedropathing.follower.Follower;
 import com.pedropathing.geometry.BezierLine;
 import com.pedropathing.geometry.Pose;
@@ -7,9 +8,17 @@ import com.pedropathing.paths.Path;
 import com.pedropathing.paths.PathChain;
 import com.pedropathing.util.Timer;
 import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
-import  com.qualcomm.robotcore.eventloop.opmode.OpMode;
-
+import org.firstinspires.ftc.teamcode.PedroPathing.Constants;
+import dev.nextftc.core.components.BindingsComponent;
 import dev.nextftc.ftc.NextFTCOpMode;
+import dev.nextftc.extensions.pedro.*;
+import static dev.nextftc.extensions.pedro.PedroComponent.follower;
+import dev.nextftc.extensions.pedro.PedroComponent;
+import dev.nextftc.core.commands.Command;
+import dev.nextftc.core.commands.delays.Delay;
+import dev.nextftc.core.commands.groups.ParallelGroup;
+import dev.nextftc.core.commands.groups.SequentialGroup;
+import dev.nextftc.core.components.SubsystemComponent;
 
 //Auton Naming Convention
 //total slots = 4: __ __ __ __
@@ -26,5 +35,99 @@ import dev.nextftc.ftc.NextFTCOpMode;
 
 @Autonomous(name = "Auton Red Far Backup", group = "Auton")
 public class AutonRedFarBackup extends NextFTCOpMode {
+    public AutonRedFarBackup() {
+        addComponents(
+                new PedroComponent(Constants::createFollower)
+        );
+    }
+    /**
+     * These change the states of the paths and actions. It will also reset the timers of the individual switches
+     **/
+    public void setPathState(int pState) {
+        pathState = pState;
+        pathTimer.resetTimer();
+    }
 
+    private Timer pathTimer, actionTimer, opmodeTimer;
+    private int pathState;
+
+    /////////////////
+    ////Positions////
+    /////////////////
+    // Positions the robot will be in during Auton
+    private final Pose startPose = new Pose(118, 132, Math.toRadians(216)); // Start Pose of our robot.
+    private final Pose endPose = new Pose(108, 120, Math.toRadians(270)); // End Pose of our robot.
+
+    /////////////
+    ////Paths////
+    /////////////
+    // The different paths the robot will take in during Auton
+    private Path robotPark;
+
+    ////////////////////
+    ////Path Builder////
+    ////////////////////
+    // Builds the aforementioned paths with the initialized positions
+    public void buildPaths() {
+        /* This is our scorePreload path. We are using a BezierLine, which is a straight line. */
+        robotPark = new Path(new BezierLine(startPose, endPose));
+        robotPark.setLinearHeadingInterpolation(startPose.getHeading(), endPose.getHeading());
+    }
+
+
+
+    /////////////////////
+    ////State Manager////
+    /////////////////////
+    // Controls which path the robot will take after finishing a specific path.
+    public void autonomousPathUpdate() {
+        switch (pathState) { //Although there may be no unique states useful for future Autons
+            case 0:
+                new FollowPath(robotPark);
+                setPathState(-1); //Last Path
+                break;
+            case 1:
+                // shouldn't do stuff
+                break;
+        }
+    }
+
+    /** This is the main loop of the OpMode, it will run repeatedly after clicking "Play". **/
+    @Override
+    public void runOpMode() {
+        // These loop the movements of the robot, these must be called continuously in order to work
+//        follower.update();
+        autonomousPathUpdate();
+        // Feedback to Driver Hub for debugging
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower().getPose().getX());
+        telemetry.addData("y", follower().getPose().getY());
+        telemetry.addData("heading", follower().getPose().getHeading());
+        telemetry.update();
+    }
+    /** This method is called once at the init of the OpMode. **/
+    @Override
+    public void onInit() {
+        pathTimer = new Timer();
+        opmodeTimer = new Timer();
+        opmodeTimer.resetTimer();
+//        follower = Constants.createFollower(hardwareMap);
+        buildPaths();
+//        follower.setStartingPose(startPose);
+    }
+
+    /** This method is called continuously after Init while waiting for "play". **/
+    @Override
+    public void onWaitForStart() {}
+
+    /** This method is called once at the start of the OpMode.
+     * It runs all the setup actions, including building paths and starting the path system **/
+    @Override
+    public void onStartButtonPressed() {
+        opmodeTimer.resetTimer();
+        setPathState(0);
+    }
+    /** We do not use this because everything should automatically disable **/
+    @Override
+    public void onStop() {}
 }
