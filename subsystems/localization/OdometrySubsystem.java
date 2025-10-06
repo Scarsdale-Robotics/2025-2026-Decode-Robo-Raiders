5225fb13e3742f344175d315915ad61df5dc7e61
Arjan <bawejaa35@gmail.com>
2025-10-05 21:18:32 -0400

    a boar is attacking me

--- subsystems/localization/OdometrySubsystem.java
+++ subsystems/localization/OdometrySubsystem.java
@@ -1,10 +1,22 @@
 package org.firstinspires.ftc.teamcode.subsystems.localization;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.GoBildaPinpointDriver;
+
 import dev.nextftc.core.subsystems.Subsystem;
 
-public class OdometrySubsystem implements Subsystem {
-  public static final OdometrySubsystem INSTANCE = new OdometrySubsystem();
+public class OdometrySubsystem {
 
-  // runs each loop
-  public void onUpdate(){}
+
+  public double ROx1;
+  public double ROy1;
+  public double ROh; //normalized to [pi, -pi)
+  public double distance;
+  public GoBildaPinpointDriver pinpoint;
+
+  public OdometrySubsystem(double x1, double y1, double h, HardwareMap hm){
+    pinpoint.update();
+    pinpoint.getPosY();
+  }
 }
--- subsystems/localization/kalman.java
+++ subsystems/localization/kalman.java
@@ -1,8 +1,15 @@
 package org.firstinspires.ftc.teamcode.subsystems.localization;
 
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
+import org.firstinspires.ftc.teamcode.GoBildaPinpointDriver;
+
 public class kalman {
 
-    public void kalman(){
 
-    }
+
+
+
+
+
 }
