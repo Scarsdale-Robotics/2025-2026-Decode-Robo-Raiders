8137f2ee270fe5181cd75c1b9d6284f7f157d33a
Arjan <bawejaa35@gmail.com>
2025-10-11 10:03:04 -0400

    Our methods for measuring time have been around for a long, long time! 4,000 years ago, Ancient Egyptians and Babylonians introduced the duodecimal system, based on the number 12. This is why we still have 12 months in a year, 24 hours in a day, and 60 minutes in an hour

--- subsystems/LocalizationSubsystem.java
+++ subsystems/LocalizationSubsystem.java
@@ -1,12 +1,9 @@
 package org.firstinspires.ftc.teamcode.subsystems;
 
 import com.qualcomm.robotcore.hardware.HardwareMap;
-
-import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.subsystems.localization.CVSubsystem;
 import org.firstinspires.ftc.teamcode.subsystems.localization.Kalman;
 import org.firstinspires.ftc.teamcode.subsystems.localization.OdometrySubsystem;
-
 
 public class LocalizationSubsystem {
 
@@ -19,22 +16,33 @@ public class LocalizationSubsystem {
   double Ax;
   double Ay;
   double Ah;
-  CVSubsystem cv; //note rember to have a method to return mosique
+  CVSubsystem cv;
   OdometrySubsystem odom;
   Kalman kalmanX;
   Kalman kalmanY;
 
-  public LocalizationSubsystem(double x1, double y1, double h, boolean side, HardwareMap hm){
+  //timing stuff
+  double clock;
+  double timeSinceLastUpdate;
+  double lastUpdateTime;
+
+  public LocalizationSubsystem(double x1, double y1, double h, boolean side, HardwareMap hm) {
     cv = new CVSubsystem(x1, y1, h, side, hm);
     odom = new OdometrySubsystem(x1, y1, h, hm);
-    kalmanX = new Kalman(x1, 0.05, 0.3, 0.7); //tune - (0.3q = 70% trust odom)
-    kalmanY = new Kalman(y1,0.05, 0.3, 0.7 ); //tune - (0.3q = 70% trust odom)
+    kalmanX = new Kalman(x1, 0.05, 0.3, 0.7);// tune - (0.3q = 70% trust odom)
+    kalmanY = new Kalman(y1, 0.05, 0.3, 0.7);// tune - (0.3q = 70% trust odom
     Rx = kalmanX.getEstimate();
     Ry = kalmanY.getEstimate();
     Rh = odom.getROh();
+    clock = System.currentTimeMillis();
+    lastUpdateTime = clock;
+    timeSinceLastUpdate = 0;
   }
 
-  public void update(){
+  public void update() {
+    clock = System.currentTimeMillis();
+    timeSinceLastUpdate = clock - lastUpdateTime;
+    lastUpdateTime = clock;
     cv.updateCV();
     odom.updateOdom();
     kalmanX.updateKF(odom.getROx1(), cv.getRCx1());
@@ -44,22 +52,18 @@ public class LocalizationSubsystem {
     Rh = odom.getROh();
   }
 
-
-
-
-
-
-  ///gets/setters///
   public double getX() { return Rx; }
   public double getY() { return Ry; }
   public double getH() { return Rh; }
-  public double getVX() { return 0.0; }
-  public double getVY() { return 0.0; }
-  public double getVH() { return 0.0; }
-  public double getAX() { return 0.0; }
-  public double getAY() { return 0.0; }
-  public double getAH() { return 0.0; }
-  public void setPos(double x1, double y1, double h){cv.setCv(x1, y1, h); odom.setPinpoint(x1, y1, h);}
-  public CVSubsystem.motif getMotif() {return cv.getMotifc();}
-
+  public double getVX() { return Vx; }
+  public double getVY() { return Vy; }
+  public double getVH() { return Vh; }
+  public double getAX() { return Ax; }
+  public double getAY() { return Ay; }
+  public double getAH() { return Ah; }
+  public double getTimeSinceLastUpdate() { return timeSinceLastUpdate; }
+  public double getClock() { return clock; }
+  private double getTimeS() { return System.nanoTime() / 1_000_000.0; }
+  public void setPos(double x1, double y1, double h) { cv.setCv(x1, y1, h); odom.setPinpoint(x1, y1, h); }
+  public CVSubsystem.motif getMotif() { return cv.getMotifc(); }
 }
