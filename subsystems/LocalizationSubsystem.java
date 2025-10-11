194fbe30047146c8eab603d6045fe1f5a33cdb0b
Arjan Baweja <arjan_baweja@MacBookAir>
2025-10-10 20:43:37 -0400

    A Kalman filter at the highest level is an algorithm that optimally estimates any given state of a system, given a model of how the system changes over time and knowing a set of sensor measurements. We use a Kalman filter whenever we have doubts about the quality of our sensors, and we require more reliable measurements to control our system with the performance that we desire.

--- subsystems/LocalizationSubsystem.java
+++ subsystems/LocalizationSubsystem.java
@@ -3,71 +3,31 @@ package org.firstinspires.ftc.teamcode.subsystems;
 import org.firstinspires.ftc.robotcore.external.navigation.Pose2D;
 import org.firstinspires.ftc.teamcode.subsystems.localization.CVSubsystem;
 
-import dev.nextftc.core.commands.Command;
-import dev.nextftc.core.commands.utility.InstantCommand;
-import dev.nextftc.core.subsystems.SubsystemGroup;
-
-// CAUTION !! RESETTING LOCALIZATION SHOULD HANDLE (or at least keep note of) POTENTIAL VELOCITY SPIKE !!
-public class LocalizationSubsystem extends SubsystemGroup {
-
-  private double x;
-  private double y;
-  private double h;
 
 
+public class LocalizationSubsystem {
 
-  public static final LocalizationSubsystem INSTANCE = new LocalizationSubsystem();
-  private LocalizationSubsystem() {
-    super(
-
-    );
-  }
+  double Rx;
+  double Ry;
+  double Rh;
+  double Vx;
+  double Vy;
+  double Vh;
+  double Ax;
+  double Ay;
+  double Ah;
 
 
 
-  public int getPattern(){
-    // returns motif pattern
-    return 0;
-  }
 
 
 
-  public double getX(){
-    return x; // make this work eventually
-  }
-  public double getY(){
-    return y; // make this work eventually
-  }
-  public double getH(){
-    return h; // make this work eventually
-  }
-  public double getVX(){
-    return x; // make this work eventually
-  }
-  public double getVY(){
-    return y; // make this work eventually
-  }
-  public double getVH(){
-    return h; // make this work eventually
-  }
-  public Command resetX(double X){
-    return new InstantCommand(() -> {
+  public double getX() { return 0.0; }
+  public double getY() { return 0.0; }
+  public double getH() { return 0.0; }
+  public double getVX() { return 0.0; }
+  public double getVY() { return 0.0; }
+  public double getVH() { return 0.0; }
 
-    });
-  }
-  public Command resetY(double Y){
-    return new InstantCommand(() -> {
 
-    });
-  }
-  public Command resetH(double H){
-    return new InstantCommand(() -> {
-
-    });
-  }
-
-  @Override
-  public void periodic(){
-    // update headings based on locale modules
-  }
 }
