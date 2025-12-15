bb08c20675518e5574eb2570de6669a6356824e8
Sira0909 <sira6060@proton.me>
2025-12-15 15:53:06 -0500

    fixing color sensor

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/localization/MagazineColorSensors.kt
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subsystems/localization/MagazineColorSensors.kt
@@ -9,25 +9,29 @@ enum class BallColor{
   PURPLE
 }
 class MagazineColorSensors(val sensorFront: ColorSensor, val sensorBack: ColorSensor) {
-  private double[2] GreenHue;
-  private double GreenSat;
-  private double GreenVal;
-  private double[2] PurpleHue;
-  private double PurpleSat;
-  private double PurpleVal;
+  //tune
+  private val GreenHue = arrayOf(70, 170)
+  private val GreenSat = 40
+  private val GreenVal = 35
+  private val PurpleHue = arrayOf(250, 300)
+  private val PurpleSat =40
+  private val PurpleVal=50
 
   fun BallOrder() : Array<BallColor>{
     val front32 = sensorFront.argb()
+    val back32  = sensorBack.argb()
+    val detections =arrayOf(Detect(front32), Detect(back32))
+    return detections
   }
 
   private fun Detect( color: Int): BallColor{
     val hsv = FloatArray(3)
     Color.colorToHSV(color,hsv)
     if(GreenHue[0]<hsv[0] && GreenHue[1]>hsv[0] && GreenSat < hsv[1] && GreenVal < hsv[2]) {
-      return GREEN;
+      return GREEN
     }
     if(PurpleHue[0]<hsv[0] && PurpleHue[1]>hsv[0] && PurpleSat < hsv[1] && PurpleVal < hsv[2]) {
-      return PURPLE;
+      return PURPLE
     }
     return NONE
     //// I hate not having pointers
