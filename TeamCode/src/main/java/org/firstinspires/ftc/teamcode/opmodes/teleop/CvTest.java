a9cb0310b2957dc15555586a3da5c798d3f28fac
arjan <bawejaa35@gmail.com>
2026-02-13 13:05:52 -0500

    pfishcys

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/CvTest.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode.opmodes.teleop;
+
+import com.bylazar.telemetry.TelemetryManager;
+import com.bylazar.telemetry.TelemetryPluginConfig;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+import org.firstinspires.ftc.teamcode.subsystems.localization.CVSubsystem_VisionPortal;
+
+import java.util.List;
+
+import kotlin.Unit;
+
+public class CvTest extends LinearOpMode {
+
+    CVSubsystem_VisionPortal cv;
+    private TelemetryManager panelsManager;
+
+    @Override
+    public void runOpMode() throws InterruptedException {
+        cv = new CVSubsystem_VisionPortal(0,0,Math.PI/2, hardwareMap); //starting pose
+        panelsManager = new TelemetryManager(
+                () -> new TelemetryPluginConfig(), //defualt config
+                (List<String> list) -> Unit.INSTANCE,
+                (Long interval) -> Unit.INSTANCE
+        );
+
+        waitForStart();
+        while(opModeIsActive()){
+            cv.updateCV();
+            panelsManager.addLine("CV");
+            panelsManager.addData("xc: ", cv.getRCx1());
+            panelsManager.addData("yc: ", cv.getRCy1());
+            panelsManager.addData("yc: ", cv.getRCh());
+            panelsManager.update(telemetry);
+        }
+    }
+}
