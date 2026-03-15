b994398540d7769167e48fc91934b3a1345861db
Arjan <bawejaa35@gmail.com>
2026-03-15 19:38:19 -0400

    asdhashda

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/CvTest/TwoPTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/CvTest/TwoPTest.java
@@ -4,6 +4,7 @@ import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.hardware.HardwareMap;
 
 import org.firstinspires.ftc.teamcode.subsystems.cv.CvPortalManager;
+import org.firstinspires.ftc.vision.opencv.Circle;
 import org.firstinspires.ftc.vision.opencv.ColorBlobLocatorProcessor;
 import org.opencv.core.Mat;
 
@@ -17,11 +18,51 @@ public class TwoPTest extends LinearOpMode {
     Double h;
     List<ColorBlobLocatorProcessor.Blob> Blobs;
 
-    Blob close;
+    ColorBlobLocatorProcessor.Blob close;
     Blob big;
     @Override
     public void runOpMode() throws InterruptedException {
         this.Portal = new CvPortalManager(72,72,-Math.PI, true, hardwareMap);
-        x = Portal.aprilTag.getX1();
+        x = Portal.getX2();
+        y = Portal.getY2();
+        h = Portal.getH2();
+
+        waitForStart();
+        while(opModeIsActive()){
+            x = Portal.getX2();
+            y = Portal.getY2();
+            h = Portal.getH2();
+
+            this.Blobs = Portal.getBlobs1();
+            double min = Double.MAX_VALUE;
+            double max = Double.MIN_VALUE;
+
+            if (Blobs != null && !Blobs.isEmpty()) {
+                telemetry.addData("#Blobs", Blobs.size());
+                for (ColorBlobLocatorProcessor.Blob b : Blobs) {
+                    Circle circleFit = b.getCircle();
+                    if (circleFit == null) continue;
+                    double radius = circleFit.getRadius();
+                    if (radius == 0) continue;
+                    double cd = (120.0 * 391) / (radius * 2);
+                    double theta = Math.atan2(circleFit.getX() - 320, 391);
+                    telemetry.addData("Blob @ X=" + (int)circleFit.getX() + " Circularity", b.getCircularity());
+                    telemetry.addData("Blob @ X=" + (int)circleFit.getX() + " Radius", radius);
+                    telemetry.addData("Blob @ X=" + (int)circleFit.getX() + " Y", circleFit.getY());
+                    telemetry.addData("Blob @ X=" + (int)circleFit.getX() + " Distance", cd);
+                    telemetry.addData("Blob @ X=" + (int)circleFit.getX() + " Angle", theta);
+
+                    if (cd < min) {
+                        min = cd;
+                        close = b;
+                    }
+
+
+                }
+            }
+
+            Portal.update();
+            telemetry.update();
+        }
     }
 }
