0627a06e615a9e7c140dded12977dde38d366604
arjan <bawejaa35@gmail.com>
2026-03-28 12:07:39 -0400

    triangles :

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/LocalTests/InShootingZoneTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/opmodes/teleop/LocalTests/InShootingZoneTest.java
@@ -1,18 +1,18 @@
 package org.firstinspires.ftc.teamcode.opmodes.teleop.LocalTests;
+import com.bylazar.configurables.annotations.Configurable;
 import com.bylazar.telemetry.TelemetryManager;
 import com.bylazar.telemetry.TelemetryPluginConfig;
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 
 import org.firstinspires.ftc.teamcode.subsystems.localization.OdometrySubsystem;
 
-import java.awt.*;
-//import java.awt.Polygon;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
 import java.util.List;
 
 import kotlin.Unit;
 
+@TeleOp(name = "RangeTest")
+@Configurable
 public class InShootingZoneTest extends LinearOpMode {
     OdometrySubsystem odom;
     private TelemetryManager panelsManager;
@@ -36,27 +36,45 @@ public class InShootingZoneTest extends LinearOpMode {
             panelsManager.addData("xo: ", odom.getROx1());
             panelsManager.addData("yo: ", odom.getROy1());
             panelsManager.addData("ho: ", odom.getROh());
+
+            int check = this.inTriangle(odom.getROx1(), odom.getROy1());
+            if (check == 0){panelsManager.addLine("None");}
+            if (check == 1){panelsManager.addLine("Top");}
+            if (check == 2){panelsManager.addLine("Bottom");}
+            if (check == 3){panelsManager.addLine("Error");}
+
+            panelsManager.update();
         }
 
 
     }
 
-    int[] x_top = {72,-8,152};
-    int[] y_top = {64,144,144};
-    int[] x_bottom = {72,40,104};
-    int[] y_bottom = {32,0,0};
+//    double[] x_top = {72,-8,152};
+//    double[] y_top = {64,144,144};
+//    double[] x_bottom = {72,40,104};
+//    double[] y_bottom = {32,0,0};
 
 
 
-    Polygon top = new Polygon(x_top, y_top);
-    Polygon 
-    ///0 = none, 1 = top, 2 = bottom, 3 = error
+
     public int inTriangle(double x1, double y1){
-        if(x1>144 || x1<0 || y1>144 || y1<0){
-            return 0;
+        ///0 = none, 1 = top, 2 = bottom, 3 = error
+        if (x1 > 144 || x1 < 0 || y1 > 144 || y1 < 0) {
+            return 3;
         }
-        else if
 
-        return 1;
+        // T triangle: vertices (72,64), (-8,144), (152,144)
+        boolean inTop = (y1 <= -x1 + 136) &&
+                (y1 <= x1 - 8)    &&
+                (y1 <= 144);
+
+        // B triangle:  (40,0), (72,32), (104,0)
+        boolean inBottom = (y1 <= x1 - 40)   &&
+                (y1 <= -x1 + 104) &&
+                (y1 >= 0);
+
+        if (inTop)    return 1;
+        if (inBottom) return 2;
+        return 0;
     }
 }
